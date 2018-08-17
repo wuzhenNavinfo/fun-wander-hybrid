f670d6f75ad46ea92ddbca57c15f50e2a37ac00c
ChenXiao <chenxiaoxiao@navinfo.com>
2018-08-17 11:19:08 +0800

    修改静态文件目录

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     outputDir: 'widget', // apicloud指定的目录
-    // assetsDir: 'static', // 静态文件目录
+    assetsDir: 'static', // 静态文件目录
     devServer: {
         // 解决跨域问题，代理服务接口请求
         proxy: {
