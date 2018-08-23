959805c1bd5d15b3b394e2cb45cc8fc3f28d8fa5
warriorsword1989 <linglong@navinfo.com>
2018-08-23 14:07:39 +0800

    修改leaflet.vue布局

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,7 +1,9 @@
 <template>
-    <div id="leafletMap" class="map-container">
+    <div class="container">
         <app-header></app-header>
-        <router-view></router-view>
+        <div id="leafletMap" class="map-container">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
@@ -248,10 +250,11 @@
     .container {
         width: 100%;
         height: 100%;
-        position: relative;
+        display: flex;
+        flex-direction: column;
     }
     .map-container {
         width: 100%;
-        height: 100%;
+        flex: 1;
     }
 </style>
